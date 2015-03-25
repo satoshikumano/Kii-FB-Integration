70ffca2eebfa7c64b997be04fc75a36d21c3e967
satoshi <satoshi.kumano@kii.com>
2015-03-25 12:53:57 +0900

    Integrated with Kii.

--- app/src/main/java/com/pgtest/example/kii/fb3_study/MainActivity.java
+++ app/src/main/java/com/pgtest/example/kii/fb3_study/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.LoggingBehavior;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.Settings;
-
+import com.kii.cloud.storage.Kii;
+import com.kii.cloud.storage.KiiUser;
+import com.kii.cloud.storage.callback.KiiSocialCallBack;
+import com.kii.cloud.storage.social.KiiFacebookConnect;
+import com.kii.cloud.storage.social.KiiSocialConnect;
 
 public class MainActivity extends ActionBarActivity implements  Session.StatusCallback {
 
@@ -22,6 +25,7 @@ public class MainActivity extends ActionBarActivity implements  Session.StatusCa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Kii.initialize("9ab34d8b", "7a950d78956ed39f3b0815f0f001b43b", Kii.Site.JP);
         setContentView(R.layout.activity_main);
 
         Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
@@ -61,18 +65,50 @@ public class MainActivity extends ActionBarActivity implements  Session.StatusCa
         super.onSaveInstanceState(outState);
         Session session = Session.getActiveSession();
         Session.saveSession(session, outState);
+
+        Kii.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Kii.onRestoreInstanceState(savedInstanceState);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+        if (requestCode == KiiFacebookConnect.REQUEST_CODE) {
+            Kii.socialConnect(KiiSocialConnect.SocialNetwork.FACEBOOK)
+                    .respondAuthOnActivityResult(requestCode, resultCode, data);
+        }
     }
 
     private void updateView(final Session session) {
         if (session.isOpened()) {
-            textView.setText("https://graph.facebook.com/me/friends?access_token=" + session
-                    .getAccessToken());
+
+            // FB login succeeded. Login to Kii with obtained access token.
+            KiiFacebookConnect kfb = (KiiFacebookConnect) Kii.socialConnect(KiiSocialConnect
+                    .SocialNetwork.FACEBOOK);
+            kfb.initialize("210344862421225", null, null);
+            Bundle options = new Bundle();
+            String accessToken = session.getAccessToken();
+            options.putString("access_token", accessToken);
+
+            Kii.socialConnect(KiiSocialConnect.SocialNetwork.FACEBOOK).logIn(this, options,
+                    new KiiSocialCallBack() {
+                        @Override
+                        public void onLoginCompleted(KiiSocialConnect.SocialNetwork network, KiiUser
+                                user, Exception exception) {
+                            if (exception != null) {
+                                textView.setText("Failed to Login to Kii! " + exception
+                                        .getMessage());
+                            }
+                            textView.setText("Login to Kii! " + user.getDisplayname());
+                        }
+                    });
+
             button.setText("LOGOUT");
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -81,6 +117,7 @@ public class MainActivity extends ActionBarActivity implements  Session.StatusCa
                     if (!session.isClosed()) {
                         session.closeAndClearTokenInformation();
                     }
+                    KiiUser.logOut();
                 }
             });
         } else {
