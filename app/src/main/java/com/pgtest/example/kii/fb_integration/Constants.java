8cac44a826d39d1dc939b716d5b92b4d053bff68
satoshi <satoshi.kumano@kii.com>
2016-05-24 19:05:18 +0900

    Introduced Constants class.

---
+++ app/src/main/java/com/pgtest/example/kii/fb_integration/Constants.java
@@ -0,0 +1,9 @@
+package com.pgtest.example.kii.fb_integration;
+
+import com.kii.cloud.storage.Kii.Site;
+
+public class Constants {
+    public static final String Kii_APP_ID = "9ab34d8b";
+    public static final String KII_APP_KEY = "7a950d78956ed39f3b0815f0f001b43b";
+    public static final Site KII_SITE = Site.JP;
+}
